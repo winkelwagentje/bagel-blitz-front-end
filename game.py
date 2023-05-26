1bc085ba177e0cbc8fca0b9eb0ef3b9a57d347d0
winkelwagentje <jui.yvesvanhaaren@gmail.com>
2023-05-26 18:12:24 +0200

    square_is_empty implemented and a start for is_valid_move has been made

--- game.py
+++ game.py
@@ -17,9 +17,45 @@ class Game:
         self.pieces.append(PieceLogic("WK", 2, 7))
 
     def square_is_empty(self, selected_square):  # returns true iff there is no piece occupying the square
-        raise Exception("square_is_empty is not implemented yet")
+        for piecelogic in self.pieces:
+            if piecelogic.x == selected_square.x and piecelogic.y == selected_square.y:
+                return False
+        return True
 
     def is_valid_move(self, from_square, to_square):  # returns true iff the move requested is allowed
+        piececode = ""
+        for piecelogic in self.pieces:
+            if piecelogic.x == from_square.x and piecelogic.y == from_square.y:
+                piececode = piecelogic.code
+                break
+        match piececode:
+            case "BB":
+                #still need to check if a move doesnt put its own king in check.
+                if abs(to_square.x - from_square.x) == abs(to_square.y - from_square.y) and 0 <= to_square.x <= 7 and 0 <= to_square.y <= 7:
+                    return True
+                return False
+            case "BK":
+                raise Exception("not implemented yet")
+            case "BN":
+                raise Exception("not implemented yet")
+            case "BP":
+                raise Exception("not implemented yet, also watch en passent")
+            case "BQ":
+                raise Exception("not implemented yet")
+            case "BR":
+                raise Exception("not implemented yet")
+            case "WB":
+                raise Exception("not implemented yet")
+            case "WK":
+                raise Exception("not implemented yet")
+            case "WN":
+                raise Exception("not implemented yet")
+            case "WP":
+                raise Exception("not implemented yet")
+            case "WQ":
+                raise Exception("not implemented yet")
+            case "WR":
+                raise Exception("not implemented yet")
         raise Exception("is_valid_move is not implemented yet")
 
     def move(self, from_square, to_square):  # moves a piece from from_square to to_square
